2f879b764b717b8b139e5d145dd47b7cb3300865
Will <wwelsh24@gmail.com>
2023-04-19 18:01:07 -0400

    fixing lineup bug

--- scripts/home_script.js
+++ scripts/home_script.js
@@ -3,7 +3,7 @@ function parseCsv_Lineup(d) {
     return {
         "Team":d.TEAM_NAME,
         "Lineup":d.GROUP_NAME,
-        "Team_ID": d.TEAM_ID,
+        "Team_ID": d.TEAM_ABBREVIATION,
         "Wins": d.W,
         "Losses": d.L,
         "Minutes":+d.MIN,
